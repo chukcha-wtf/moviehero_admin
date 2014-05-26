96c5ab85b5847295dbbe64e618d017bfbf7999fe
Pavlo Babenko <pavlo.babenko@gmail.com>
2014-05-26 17:48:28 +0300

    Hacking activerecord

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,8 +2,12 @@ class Ticket < ActiveRecord::Base
   self.table_name = 'Ticket'
   self.inheritance_column = '_type'
 
-  has_many :order_items, -> { where type: 'ticket' }, :foreign_key => 'foreignId'
+  # has_many :order_items, -> { where type: 'ticket' }, :foreign_key => 'foreignId'
+  # has_many :order_items, as: :orderable, :foreign_key => 'foreignId', :foreign_type => 'type'
   has_many :ticket_numbers, :foreign_key => 'ticketId', :class_name => 'TicketNumber'
+  has_many :order_items, -> {
+    where(type: 'ticket')
+  }, :foreign_key => 'foreignId'
 
   belongs_to :showing, :foreign_key => 'showingId'
   belongs_to :memebr, :foreign_key => 'memberId', :class_name => 'User'
