13459d87ab0ca95f472f58d10af57de8366cc70e
Artem Pechenyi <pechkinator@gmail.com>
2014-05-26 18:39:44 +0300

    polymorphic_belongs_to virtual type column

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -4,6 +4,30 @@ class OrderItem < ActiveRecord::Base
 
   belongs_to :order, :foreign_key => 'orderId'
 
-  belongs_to :orderable, polymorphic: true, :foreign_key => 'foreignId', foreign_type: 'type'
-
+  belongs_to :orderable, polymorphic: true, :foreign_key => 'foreignId'
+  
+  def read_attribute attr_name
+    if attr_name.to_sym == :orderable_type
+      self.class.write_polymorphic_type(super(:type))
+    else
+      super(attr_name)
+    end
+  end
+  
+  def write_attribute attr_name, attr_value
+    if attr_name.to_sym == :orderable_type
+      super(:type, self.class.read_polymorphic_type(attr_value))
+    else
+      super(attr_name, attr_value)
+    end
+  end
+  
+  def self.read_polymorphic_type type
+    type.capitalize
+  end
+  
+  def self.write_polymorphic_type type
+    type.underscore
+  end
+  
 end
